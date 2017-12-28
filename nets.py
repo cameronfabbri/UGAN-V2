6608eabe6735ae6b65a3f7f8505acd5a67b5b2c0
Cameron Fabbri <cameronfabbri@gmail.com>
2017-12-28 14:51:04 -0500

    finished the network

--- nets.py
+++ nets.py
@@ -9,9 +9,9 @@ from tf_ops import *
 def resblock(x, dim, name, ks=3, s=1):
 
    p = int((ks - 1) / 2)
-   x = tf.pad(x, [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
+   x_ = tf.pad(x, [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
  
-   y = tcl.conv2d(x, dim, ks, s, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_res1'+str(name), padding='VALID')
+   y = tcl.conv2d(x_, dim, ks, s, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_res1'+str(name), padding='VALID')
    y = instance_norm(y)
    y = tf.pad(relu(y), [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
    
@@ -22,58 +22,61 @@ def resblock(x, dim, name, ks=3, s=1):
 
 '''
    Architecture from https://arxiv.org/pdf/1703.10593.pdf
+   but really from StarGAN
 '''
 def netG(x, LOSS_METHOD, INSTANCE_NORM=True, PIXEL_SHUF=False):
 
-   # c7s1-32
-   enc_conv1 = tcl.conv2d(x, 32, 7, 1, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv1')
+   # c7s1-64
+   enc_conv1 = tcl.conv2d(x, 64, 7, 1, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv1')
    if INSTANCE_NORM: enc_conv1 = instance_norm(enc_conv1)
    else: enc_conv1 = tcl.batch_norm(enc_conv1)
    enc_conv1 = relu(enc_conv1)
    
-   # d64
-   enc_conv2 = tcl.conv2d(enc_conv1, 64, 3, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv2')
+   # d128
+   enc_conv2 = tcl.conv2d(enc_conv1, 128, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv2')
    if INSTANCE_NORM: enc_conv2 = instance_norm(enc_conv2)
    else: enc_conv2 = tcl.batch_norm(enc_conv2)
    enc_conv2 = relu(enc_conv2)
    
-   # d128
-   enc_conv3 = tcl.conv2d(enc_conv2, 128, 3, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv3')
+   # d256
+   enc_conv3 = tcl.conv2d(enc_conv2, 256, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv3')
    if INSTANCE_NORM: enc_conv3 = instance_norm(enc_conv3)
    else: enc_conv3 = tcl.batch_norm(enc_conv3)
    enc_conv3 = relu(enc_conv3)
 
-   re = resblock(enc_conv3, 128, 'r1')
-   re = resblock(re, 128, 'r2')
-   re = resblock(re, 128, 'r3')
-   re = resblock(re, 128, 'r4')
-   re = resblock(re, 128, 'r5')
-   re = resblock(re, 128, 'r6')
-   re = resblock(re, 128, 'r7')
-   re = resblock(re, 128, 'r8')
-   re = resblock(re, 128, 'r9')
+   re = resblock(enc_conv3, 256, 'r1')
+   re = resblock(re, 256, 'r2')
+   re = resblock(re, 256, 'r3')
+   re = resblock(re, 256, 'r4')
+   re = resblock(re, 256, 'r5')
+   re = resblock(re, 256, 'r6')
+   re = resblock(re, 256, 'r7')
+   re = resblock(re, 256, 'r8')
+   re = resblock(re, 256, 'r9')
 
    print 'x:        ',x
    print 'enc_conv1:',enc_conv1
    print 'enc_conv2:',enc_conv2
    print 'enc_conv3:',enc_conv3
    print 're:',re
-   exit()
-   print 'enc_conv2:',enc_conv2
-   print 'enc_conv3:',enc_conv3
-   print 'enc_conv4:',enc_conv4
-   print 'enc_conv5:',enc_conv5
-   print 'enc_conv6:',enc_conv6
-   print 'enc_conv7:',enc_conv7
-   print 'enc_conv8:',enc_conv8
    print
-
-   dec_conv1 = tcl.convolution2d_transpose(enc_conv8, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv1')
+   
+   dec_conv1 = tcl.convolution2d_transpose(re, 128, 3, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv1')
+   if INSTANCE_NORM: dec_conv1 = instance_norm(dec_conv1)
    dec_conv1 = relu(dec_conv1)
-   dec_conv1 = tf.concat([dec_conv1, enc_conv7], axis=3)
    print 'dec_conv1:',dec_conv1
-
-   return dec_conv8
+   
+   dec_conv2 = tcl.convolution2d_transpose(dec_conv1, 64, 3, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv2')
+   if INSTANCE_NORM: dec_conv2 = instance_norm(dec_conv2)
+   dec_conv2 = relu(dec_conv2)
+   print 'dec_conv2:',dec_conv2
+   
+   out = tcl.convolution2d_transpose(dec_conv2, 3, 7, 1, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_out')
+   if INSTANCE_NORM: out = instance_norm(out)
+   dec_conv2 = relu(out)
+   print 'out:',out
+   exit()
+   return out
 
 
 def netD(x, LAYER_NORM, LOSS_METHOD, reuse=False):
