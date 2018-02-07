bf41e22405c90aacef0a18cc5ec9fa465454ed67
Cameron Fabbri <cameronfabbri@gmail.com>
2018-02-07 15:56:59 -0600

    got most started

--- faderNets/nets/pix2pix.py
+++ faderNets/nets/pix2pix.py
@@ -5,25 +5,25 @@ import sys
 sys.path.insert(0, 'ops/')
 from tf_ops import *
 
-def netG(x, LOSS_METHOD):
+def encoder(x):
       
-   enc_conv1 = tcl.conv2d(x, 64, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv1')
+   enc_conv1 = tcl.conv2d(x, 16, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv1')
    enc_conv1 = tcl.batch_norm(enc_conv1)
    enc_conv1 = lrelu(enc_conv1)
    
-   enc_conv2 = tcl.conv2d(enc_conv1, 128, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv2')
+   enc_conv2 = tcl.conv2d(enc_conv1, 32, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv2')
    enc_conv2 = tcl.batch_norm(enc_conv2)
    enc_conv2 = lrelu(enc_conv2)
    
-   enc_conv3 = tcl.conv2d(enc_conv2, 256, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv3')
+   enc_conv3 = tcl.conv2d(enc_conv2, 64, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv3')
    enc_conv3 = tcl.batch_norm(enc_conv3)
    enc_conv3 = lrelu(enc_conv3)
 
-   enc_conv4 = tcl.conv2d(enc_conv3, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv4')
+   enc_conv4 = tcl.conv2d(enc_conv3, 128, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv4')
    enc_conv4 = tcl.batch_norm(enc_conv4)
    enc_conv4 = lrelu(enc_conv4)
    
-   enc_conv5 = tcl.conv2d(enc_conv4, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv5')
+   enc_conv5 = tcl.conv2d(enc_conv4, 256, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv5')
    enc_conv5 = tcl.batch_norm(enc_conv5)
    enc_conv5 = lrelu(enc_conv5)
 
@@ -35,10 +35,8 @@ def netG(x, LOSS_METHOD):
    enc_conv7 = tcl.batch_norm(enc_conv7)
    enc_conv7 = lrelu(enc_conv7)
 
-   enc_conv8 = tcl.conv2d(enc_conv7, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_enc_conv8')
-   enc_conv8 = tcl.batch_norm(enc_conv8)
-   enc_conv8 = lrelu(enc_conv8)
-
+   print
+   print 'encoder:'
    print 'x:        ',x
    print 'enc_conv1:',enc_conv1
    print 'enc_conv2:',enc_conv2
@@ -47,84 +45,130 @@ def netG(x, LOSS_METHOD):
    print 'enc_conv5:',enc_conv5
    print 'enc_conv6:',enc_conv6
    print 'enc_conv7:',enc_conv7
-   print 'enc_conv8:',enc_conv8
    print
+   print
+   
+   enc = {'x':x,
+        'enc_conv1':enc_conv1,
+        'enc_conv2':enc_conv2,
+        'enc_conv3':enc_conv3,
+        'enc_conv4':enc_conv4,
+        'enc_conv5':enc_conv5,
+        'enc_conv6':enc_conv6,
+        'embedding':enc_conv7
+   }
 
-   # decoder, no batch norm
-   dec_conv1 = tcl.convolution2d_transpose(enc_conv8, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv1')
+   return enc
+
+def decoder(enc, y, skip_connections, upsample):
+   print 'decoder:'
+   enc_conv1 = enc['enc_conv1']
+   enc_conv2 = enc['enc_conv2']
+   enc_conv3 = enc['enc_conv3']
+   enc_conv4 = enc['enc_conv4']
+   enc_conv5 = enc['enc_conv5']
+   enc_conv6 = enc['enc_conv6']
+   enc_conv7 = enc['embedding']
+
+   # reshape so it's batchx1x1xy_size
+   y_dim = int(y.get_shape().as_list()[-1])
+   y = tf.reshape(y, shape=[-1, 1, 1, y_dim])
+
+   # concat y onto every layer
+   enc_conv7 = conv_cond_concat(enc_conv7, y)
+
+   if upsample == 'transpose_conv':
+      dec_conv1 = tcl.convolution2d_transpose(enc_conv7, 512, 4, 1, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv1')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv1 = relu(dec_conv1)
-   dec_conv1 = tf.concat([dec_conv1, enc_conv7], axis=3)
+   if skip_connections: dec_conv1 = tf.concat([dec_conv1, enc_conv7], axis=3)
+   dec_conv1 = conv_cond_concat(dec_conv1, y)
    print 'dec_conv1:',dec_conv1
 
-   dec_conv2 = tcl.convolution2d_transpose(dec_conv1, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv2')
+   if upsample == 'transpose_conv':
+      dec_conv2 = tcl.convolution2d_transpose(dec_conv1, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv2')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv2 = relu(dec_conv2)
-   dec_conv2 = tf.concat([dec_conv2, enc_conv6], axis=3)
+   if skip_connections: dec_conv2 = tf.concat([dec_conv2, enc_conv6], axis=3)
+   dec_conv2 = conv_cond_concat(dec_conv2, y)
    print 'dec_conv2:',dec_conv2
-   
-   dec_conv3 = tcl.convolution2d_transpose(dec_conv2, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv3')
+
+   if upsample == 'transpose_conv':
+      dec_conv3 = tcl.convolution2d_transpose(dec_conv2, 256, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv3')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv3 = relu(dec_conv3)
-   dec_conv3 = tf.concat([dec_conv3, enc_conv5], axis=3)
+   if skip_connections: dec_conv3 = tf.concat([dec_conv3, enc_conv5], axis=3)
+   dec_conv3 = conv_cond_concat(dec_conv3, y)
    print 'dec_conv3:',dec_conv3
 
-   dec_conv4 = tcl.convolution2d_transpose(dec_conv3, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv4')
+   if upsample == 'transpose_conv':
+      dec_conv4 = tcl.convolution2d_transpose(dec_conv3, 128, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv4')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv4 = relu(dec_conv4)
-   dec_conv4 = tf.concat([dec_conv4, enc_conv4], axis=3)
+   if skip_connections: dec_conv4 = tf.concat([dec_conv4, enc_conv4], axis=3)
+   dec_conv4 = conv_cond_concat(dec_conv4, y)
    print 'dec_conv4:',dec_conv4
    
-   dec_conv5 = tcl.convolution2d_transpose(dec_conv4, 256, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv5')
+   if upsample == 'transpose_conv':
+      dec_conv5 = tcl.convolution2d_transpose(dec_conv4, 64, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv5')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv5 = relu(dec_conv5)
-   dec_conv5 = tf.concat([dec_conv5, enc_conv3], axis=3)
+   if skip_connections: dec_conv5 = tf.concat([dec_conv5, enc_conv3], axis=3)
+   dec_conv5 = conv_cond_concat(dec_conv5, y)
    print 'dec_conv5:',dec_conv5
 
-   dec_conv6 = tcl.convolution2d_transpose(dec_conv5, 128, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv6')
+   if upsample == 'transpose_conv':
+      dec_conv6 = tcl.convolution2d_transpose(dec_conv5, 32, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv6')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv6 = relu(dec_conv6)
-   dec_conv6 = tf.concat([dec_conv6, enc_conv2], axis=3)
+   if skip_connections: dec_conv6 = tf.concat([dec_conv6, enc_conv2], axis=3)
+   dec_conv6 = conv_cond_concat(dec_conv6, y)
    print 'dec_conv6:',dec_conv6
-   
-   dec_conv7 = tcl.convolution2d_transpose(dec_conv6, 64, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv7')
+
+   if upsample == 'transpose_conv':
+      dec_conv7 = tcl.convolution2d_transpose(dec_conv6, 16, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv7')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv7 = relu(dec_conv7)
-   dec_conv7 = tf.concat([dec_conv7, enc_conv1], axis=3)
+   if skip_connections: dec_conv7 = tf.concat([dec_conv7, enc_conv1], axis=3)
+   dec_conv7 = conv_cond_concat(dec_conv7, y)
    print 'dec_conv7:',dec_conv7
-   
-   dec_conv8 = tcl.convolution2d_transpose(dec_conv7, 3, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv8')
+
+   if upsample == 'transpose_conv':
+      dec_conv8 = tcl.convolution2d_transpose(dec_conv7, 3, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv8')
+   if upsample == 'pixel_shuffle': print 'not implemented yet'
+   if upsample == 'upconv': print 'not implemented yet'
    dec_conv8 = tanh(dec_conv8)
    print 'dec_conv8', dec_conv8
-   
+   print
    return dec_conv8
 
 
-def netD(x, LOSS_METHOD, reuse=False):
-   print
-   print 'netD'
+def netD(embedding, reuse=False):
 
-   sc = tf.get_variable_scope()
-   with tf.variable_scope(sc, reuse=reuse):
+   #sc = tf.get_variable_scope()
+   #with tf.variable_scope(sc, reuse=reuse):
 
-      conv1 = tcl.conv2d(x, 64, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_conv1')
-      if LOSS_METHOD != 'wgan': conv1 = tcl.batch_norm(conv1)
-      conv1 = lrelu(conv1)
-      
-      conv2 = tcl.conv2d(conv1, 128, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_conv2')
-      if LOSS_METHOD != 'wgan': conv2 = tcl.batch_norm(conv2)
-      conv2 = lrelu(conv2)
-      
-      conv3 = tcl.conv2d(conv2, 256, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_conv3')
-      if LOSS_METHOD != 'wgan': conv3 = tcl.batch_norm(conv3)
-      conv3 = lrelu(conv3)
-      
-      conv4 = tcl.conv2d(conv3, 512, 4, 1, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_conv4')
-      if LOSS_METHOD != 'wgan': conv4 = tcl.batch_norm(conv4)
-      conv4 = lrelu(conv4)
-      
-      conv5 = tcl.conv2d(conv4, 1, 1, 1, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_conv5')
-      if LOSS_METHOD != 'wgan': conv5 = tcl.batch_norm(conv5)
+   conv1 = tcl.conv2d(embedding, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_conv1')
+   conv1 = tcl.batch_norm(conv1)
+   conv1 = lrelu(conv1)
 
-      print 'x:',x
-      print 'conv1:',conv1
-      print 'conv2:',conv2
-      print 'conv3:',conv3
-      print 'conv4:',conv4
-      print 'conv5:',conv5
-      return conv5
+   conv1 = tcl.flatten(conv1)
 
+   fc1 = tcl.fully_connected(conv1, 512, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_fc1')
+   fc1 = lrelu(fc1)
+   
+   fc2 = tcl.fully_connected(fc1, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='d_fc2')
+   #fc2 = sig(fc2)
 
+   print 'conv1:',conv1
+   print 'fc1:',fc1
+   print 'fc2:',fc2
+
+   return fc2
--- faderNets/train.py
+++ faderNets/train.py
@@ -1,9 +1,8 @@
 '''
 
-   Main training file
+   Main training file for Fader Networks
 
 '''
-import tensorflow.contrib.layers as tcl
 import scipy.misc as misc
 import cPickle as pickle
 import tensorflow as tf
@@ -12,102 +11,139 @@ import argparse
 import random
 import ntpath
 import time
+import glob
 import sys
 import os
 
-sys.path.insert(0, '../ops/')
-sys.path.insert(0, '../')
+sys.path.insert(0, 'ops/')
+sys.path.insert(0, 'nets/')
 
-from tf_ops import *
-from nets import *
 import data_ops
-
-from config import classes
+import tf_ops
 
 if __name__ == '__main__':
 
    parser = argparse.ArgumentParser()
-   parser.add_argument('--BATCH_SIZE', required=False,help='Batch size',              type=int,default=64)
-   parser.add_argument('--DATA_DIR',   required=True, help='Directory where data is', type=str,default='./')
-   parser.add_argument('--EPOCHS',     required=False,help='Maximum training epochs', type=int,default=500)
-   parser.add_argument('--LOSS',       required=False,help='Type of GAN loss to use', type=str,default='gan')
+   parser.add_argument('--batch_size', required=False, type=int, default=64,
+                        help='Batch size to use')
+   parser.add_argument('--data_dir', required=True, type=str,
+                        help='Directory where data is')
+   parser.add_argument('--epochs',required=False, type=int, default=500,
+                        help='Maximum training epochs')
+   parser.add_argument('--ae_loss', required=False, type=str, default='l2',
+                        help='Loss for the autoencoder', choices=['l2', 'gan', 'wgan'])
+   parser.add_argument('--l1_weight', required=False, type=float, default=0.0,
+                        help='l1 weight. Primarily used when using a GAN for the ae loss')
+   parser.add_argument('--instance_norm',required=False, type=str, default=0,
+                        help='Instance norm instead of batch norm')
+   parser.add_argument('--skip_connections',required=False, type=int, default=0,
+                        help='Skip connection use in the ae')
+   parser.add_argument('--decoder_dropout',required=False, type=float, default=0.,
+                        help='Dropout in the decoder')
+   parser.add_argument('--lat_dis_dropout',required=False, type=float, default=0.3,
+                        help='Dropout in the latent discriminator')
+   parser.add_argument('--smooth_label',required=False, type=float, default=0.2,
+                        help='Smooth the label for the patch discriminator')
+   parser.add_argument('--lambda_ae',required=False, type=float, default=1.,
+                        help='Autoencoder loss coefficient')
+   parser.add_argument('--lambda_lat_dis',required=False, type=float, default=0.0001,
+                        help='Latent discriminator loss feedback coefficient')
+   parser.add_argument('--lambda_clf_dis',required=False, type=float, default=0.,
+                        help='Classifier discriminator loss feedback coefficient')
+   parser.add_argument('--lambda_schedule',required=False, type=int, default=500000,
+                        help='Progressively increase discriminators lambda (0 to disable)')
+   parser.add_argument('--upsample', required=False, type=str, default='transpose_conv',
+                        help='Type of upsampling to use in the decoder',
+                        choices=['transpose_conv', 'pixel_shuffle', 'upconv'])
+   parser.add_argument('--epoch_size',required=False, type=int, default=50000,
+                        help='Size of the epoch (0 to use number of training samples)')
+   parser.add_argument('--network',required=False, type=str, default='pix2pix',
+                        help='Network architecture to use', choices=['pix2pix', 'resnet'])
    a = parser.parse_args()
 
-   BATCH_SIZE     = a.BATCH_SIZE
-   DATA_DIR       = a.DATA_DIR
-   EPOCHS         = a.EPOCHS
-   LOSS           = a.LOSS
+   batch_size       = a.batch_size
+   data_dir         = a.data_dir
+   epochs           = a.epochs
+   ae_loss          = a.ae_loss
+   l1_weight        = a.l1_weight
+   instance_norm    = bool(a.instance_norm)
+   skip_connections = bool(a.skip_connections)
+   decoder_dropout  = a.decoder_dropout
+   lat_dis_dropout  = a.lat_dis_dropout
+   smooth_label     = a.smooth_label
+   lambda_ae        = a.lambda_ae
+   lambda_lat_dis   = a.lambda_lat_dis
+   lambda_clf_dis   = a.lambda_clf_dis
+   lambda_schedule  = a.lambda_schedule
+   upsample         = a.upsample
+   epoch_size       = a.epoch_size
+   network          = a.network
 
-   CHECKPOINT_DIR = 'checkpoints/LOSS_'+LOSS+'/C_'+str(cn)+'/'
+   CHECKPOINT_DIR = 'checkpoints/ae_loss_'+ae_loss\
+                   +'/l1_weight_'+str(l1_weight)\
+                   +'/instance_norm_'+str(instance_norm)\
+                   +'/skip_connections'+str(skip_connections)\
+                   +'/decoder_dropout_'+str(decoder_dropout)\
+                   +'/lat_dis_dropout_'+str(lat_dis_dropout)\
+                   +'/smooth_label_'+str(smooth_label)\
+                   +'/lambda_ae_'+str(lambda_ae)\
+                   +'/lambda_lat_dis_'+str(lambda_lat_dis)\
+                   +'/lambda_clf_dis_'+str(lambda_clf_dis)\
+                   +'/lambda_schedule_'+str(lambda_schedule)\
+                   +'/upsample_'+upsample+'/'\
+
    IMAGES_DIR     = CHECKPOINT_DIR+'images/'
 
    try: os.makedirs(IMAGES_DIR)
    except: pass
-   
+
    exp_info = dict()
-   exp_info['CHECKPOINT_DIR']   = CHECKPOINT_DIR
-   exp_info['BATCH_SIZE'] = BATCH_SIZE
-   exp_info['DATA_DIR']   = DATA_DIR
-   exp_info['CLASSES']    = classes
-   exp_info['EPOCHS']     = EPOCHS
-   exp_info['LOSS']       = LOSS
+   exp_info['checkpoint_dir']   = CHECKPOINT_DIR
+   exp_info['batch_size']       = batch_size
+   exp_info['data_dir']         = data_dir
+   exp_info['epochs']           = epochs
+   exp_info['ae_loss']          = ae_loss
+   exp_info['l1_weight']        = l1_weight
+   exp_info['instance_norm']    = instance_norm
+   exp_info['skip_connections'] = skip_connections
+   exp_info['decoder_dropout']  = decoder_dropout
+   exp_info['lat_dis_dropout']  = lat_dis_dropout
+   exp_info['smooth_label']     = smooth_label
+   exp_info['lambda_ae']        = lambda_ae
+   exp_info['lambda_lat_dis']   = lambda_lat_dis
+   exp_info['lambda_clf_dis']   = lambda_clf_dis
+   exp_info['lambda_schedule']  = lambda_schedule
+   exp_info['upsample']         = upsample
+   exp_info['epoch_size']       = epoch_size
+   exp_info['network']          = network
 
    exp_pkl = open(CHECKPOINT_DIR+'info.pkl', 'wb')
    data = pickle.dumps(exp_info)
    exp_pkl.write(data)
    exp_pkl.close()
 
-   print 'Loading data...'
-   train_images, train_annots, train_ids, test_images, test_annots, test_ids = data_ops.load_efigi(DATA_DIR, classes, 64)
-
-   # find y dimension
-   idx_ = np.array([1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49])
-   idx_ = np.multiply(classes[:-1], idx_)
-   idx = [x for x in idx_ if x != 0]
-   y_dim = len(idx)
-   # account for redshift attribute
-   if classes[-1] == 1: y_dim += 1
-
+   # import the correct network we are using
+   if network == 'pix2pix': from pix2pix import *
+   if network == 'resnet': from resnet import *
+   
    # placeholders for data going into the network
    global_step = tf.Variable(0, name='global_step', trainable=False)
-   real_images = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 64, 64, 3), name='real_images')
-   z           = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 100), name='z')
-   y           = tf.placeholder(tf.float32, shape=(BATCH_SIZE, y_dim), name='y')
+   x           = tf.placeholder(tf.float32, shape=(batch_size, 256, 256, 3), name='real_images')
+   y           = tf.placeholder(tf.float32, shape=(batch_size, 2), name='y')
 
-   # generated images
-   gen_images = netG(z, y, BATCH_SIZE, 64)
+   # the embedding of the image - this is a dictionary of all layers
+   enc = encoder(x)
+   embedding = enc['embedding']
 
-   # get the output from D on the real and fake data
-   errD_real = netD(real_images, y, BATCH_SIZE, LOSS, 64)
-   errD_fake = netD(gen_images, y, BATCH_SIZE, LOSS, 64, reuse=True)
+   decoded = decoder(enc, y, skip_connections, upsample)
 
-   # Important! no initial activations done on the last layer for D, so if one method needs an activation, do it
-   e = 1e-12
-   if LOSS == 'gan':
-      errD_real = tf.nn.sigmoid(errD_real)
-      errD_fake = tf.nn.sigmoid(errD_fake)
-      errG = tf.reduce_mean(-tf.log(errD_fake + e))
-      errD = tf.reduce_mean(-(tf.log(errD_real+e)+tf.log(1-errD_fake+e)))
+   # D's prediction on which class the embedding is
+   logitsD = tf.nn.softmax(netD(embedding))
 
-   if LOSS == 'lsgan':
-      errD_real = tf.nn.sigmoid(errD_real)
-      errD_fake = tf.nn.sigmoid(errD_fake)
-      errD = tf.reduce_mean(0.5*(tf.square(errD_real - 1)) + 0.5*(tf.square(errD_fake)))
-      errG = tf.reduce_mean(0.5*(tf.square(errD_fake - 1)))
+   # loss on D - cross entropy with real class y
+   errD = -tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=logitsD))
 
-   if LOSS == 'wgan':
-      # cost functions
-      errD = tf.reduce_mean(errD_real) - tf.reduce_mean(errD_fake)
-      errG = tf.reduce_mean(errD_fake)
-
-      # gradient penalty
-      epsilon = tf.random_uniform([], 0.0, 1.0)
-      x_hat = real_images*epsilon + (1-epsilon)*gen_images
-      d_hat = netD(x_hat, y, BATCH_SIZE, LOSS, 64, reuse=True)
-      gradients = tf.gradients(d_hat, x_hat)[0]
-      slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
-      gradient_penalty = 10*tf.reduce_mean((slopes-1.0)**2)
-      errD += gradient_penalty
+   errG = tf.reduce_mean(tf.nn.l2_loss(x-decoded))
 
    # tensorboard summaries
    tf.summary.scalar('d_loss', errD)
@@ -119,28 +155,12 @@ if __name__ == '__main__':
    d_vars = [var for var in t_vars if 'd_' in var.name]
    g_vars = [var for var in t_vars if 'g_' in var.name]
 
-   if LOSS == 'wgan':
-      n_critic = 5
-      beta1    = 0.0
-      beta2    = 0.9
-      lr       = 1e-4
-
-   if LOSS == 'lsgan':
-      n_critic = 1
-      beta1    = 0.5
-      beta2    = 0.999
-      lr       = 0.001
-
-   if LOSS == 'gan':
-      n_critic = 1
-      beta1    = 0.5
-      beta2    = 0.999
-      lr       = 0.0002
+   train_op = tf.train.AdamOptimizer(learning_rate=0.0002).minimize(errG+errD)
 
    # optimize G
-   G_train_op = tf.train.AdamOptimizer(learning_rate=lr,beta1=beta1,beta2=beta2).minimize(errG, var_list=g_vars, global_step=global_step)
+   #G_train_op = tf.train.AdamOptimizer(learning_rate=lr,beta1=beta1,beta2=beta2).minimize(errG, var_list=g_vars, global_step=global_step)
    # optimize D
-   D_train_op = tf.train.AdamOptimizer(learning_rate=lr,beta1=beta1,beta2=beta2).minimize(errD, var_list=d_vars)
+   #D_train_op = tf.train.AdamOptimizer(learning_rate=lr,beta1=beta1,beta2=beta2).minimize(errD, var_list=d_vars)
 
    saver = tf.train.Saver(max_to_keep=1)
    init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
@@ -155,9 +175,6 @@ if __name__ == '__main__':
 
    summary_writer = tf.summary.FileWriter(CHECKPOINT_DIR+'/'+'logs/', graph=tf.get_default_graph())
 
-   tf.add_to_collection('G_train_op', G_train_op)
-   tf.add_to_collection('D_train_op', D_train_op)
-   
    # restore previous model if there is one
    ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
    if ckpt and ckpt.model_checkpoint_path:
@@ -172,60 +189,68 @@ if __name__ == '__main__':
    ########################################### training portion
 
    step = sess.run(global_step)
-   train_len = len(train_annots)
-   test_len  = len(test_annots)
 
-   print 'train num:',train_len
-   print 'test num:',test_len
+   train_distorted_paths    = np.asarray(glob.glob('/mnt/data1/images/ugan_dataset/distorted/*.JPEG'))
+   train_nondistorted_paths = np.asarray(glob.glob('/mnt/data1/images/ugan_dataset/nondistorted/*.JPEG'))
+
+   dlen  = len(train_distorted_paths)
+   ndlen = len(train_nondistorted_paths)
+
+   # create labels: d: [0, 1], nd: [1, 0]
+   dlabels = [0, 1]
+   dlabels = np.asarray([dlabels]*dlen)
    
-   epoch_num = step/(train_len/BATCH_SIZE)
-   
-   while epoch_num < EPOCHS:
-      epoch_num = step/(train_len/BATCH_SIZE)
-      start = time.time()
+   ndlabels = [1, 0]
+   ndlabels = np.asarray([ndlabels]*ndlen)
 
-      # train the discriminator
-      for critic_itr in range(n_critic):
-         idx          = np.random.choice(np.arange(train_len), BATCH_SIZE, replace=False)
-         batch_z      = np.random.normal(0.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-         batch_y      = train_annots[idx]
-         batch_images = train_images[idx]
+   while True:
 
-         # randomly flip batch
-         #r = random.random()
-         # flip image left right
-         #if r < 0.5:
-         #   batch_images = np.fliplr(batch_images)
-         
-         #r = random.random()
-         # flip image up down
-         #if r < 0.5:
-         #   batch_images = np.flipud(batch_images)
-
-         sess.run(D_train_op, feed_dict={z:batch_z, y:batch_y, real_images:batch_images})
+      # put in batch of distorted first, then non_distorted
+      idx          = np.random.choice(np.arange(dlen), batch_size, replace=False)
+      batch_y      = dlabels[idx]
+      batch_paths  = train_distorted_paths[idx]
+      batch_images = np.empty((batch_size, 256, 256, 3), dtype=np.float32)
+      i = 0
+      for img in batch_paths:
+         img = misc.imread(img)
+         img = misc.imresize(img, (256,256))
+         img = data_ops.normalize(img)
+         batch_images[i, ...] = img
+         i += 1
       
-      # now train the generator once! use normal distribution, not uniform!!
-      idx          = np.random.choice(np.arange(train_len), BATCH_SIZE, replace=False)
-      batch_z      = np.random.normal(0.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-      batch_y      = train_annots[idx]
-      batch_images = train_images[idx]
-
-      sess.run(G_train_op, feed_dict={z:batch_z, y:batch_y, real_images:batch_images})
+      sess.run(train_op, feed_dict={x:batch_images, y:batch_y})
+      
+      idx          = np.random.choice(np.arange(ndlen), batch_size, replace=False)
+      batch_y      = ndlabels[idx]
+      batch_paths  = train_nondistorted_paths[idx]
+     
+      batch_images = np.empty((batch_size, 256, 256, 3), dtype=np.float32)
+      i = 0
+      for img in batch_paths:
+         img = misc.imread(img)
+         img = misc.imresize(img, (256,256))
+         img = data_ops.normalize(img)
+         batch_images[i, ...] = img
+         i += 1
+     
+      sess.run(train_op, feed_dict={x:batch_images, y:batch_y})
+      
       D_loss, G_loss, summary = sess.run([errD, errG, merged_summary_op],
-                                 feed_dict={z:batch_z, y:batch_y, real_images:batch_images})
+                                 feed_dict={x:batch_images, y:batch_y})
 
       summary_writer.add_summary(summary, step)
 
-      print 'epoch:',epoch_num,'step:',step,'D loss:',D_loss,'G_loss:',G_loss,'time:',time.time()-start
+      print 'step:',step,'D loss:',D_loss,'G_loss:',G_loss
       step += 1
-    
+
+
       if step%500 == 0:
          print 'Saving model...'
          saver.save(sess, CHECKPOINT_DIR+'checkpoint-'+str(step))
          saver.export_meta_graph(CHECKPOINT_DIR+'checkpoint-'+str(step)+'.meta')
 
-         idx          = np.random.choice(np.arange(test_len), BATCH_SIZE, replace=False)
-         batch_z      = np.random.normal(0.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
+         idx          = np.random.choice(np.arange(test_len), batch_size, replace=False)
+         batch_z      = np.random.normal(0.0, 1.0, size=[batch_size, 100]).astype(np.float32)
          batch_y      = test_annots[idx]
          batch_images = test_images[idx]
 
