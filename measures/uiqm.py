9c9d7774f9ea716293ab07a492d3e8323e119bae
Cameron Fabbri <cameronfabbri@gmail.com>
2018-06-21 18:21:04 -0500

    update

--- measures/uiqm.py
+++ measures/uiqm.py
@@ -58,14 +58,14 @@ def _uicm(x):
    mu_a_RG = mu_a(RG)
    mu_a_YB = mu_a(YB)
 
-   print 'mu_a_RG:',mu_a_RG
-   print 'mu_a_YB:',mu_a_YB
+   #print 'mu_a_RG:',mu_a_RG
+   #print 'mu_a_YB:',mu_a_YB
 
    s_a_RG = s_a(RG, mu_a_RG)
    s_a_YB = s_a(YB, mu_a_YB)
 
-   print 's_a_RG:',s_a_RG
-   print 's_a_YB:',s_a_YB
+   #print 's_a_RG:',s_a_RG
+   #print 's_a_YB:',s_a_YB
 
    l = math.sqrt( (math.pow(mu_a_RG,2)+math.pow(mu_a_YB,2)) )
    r = math.sqrt(s_a_RG+s_a_YB)
@@ -92,24 +92,23 @@ def eme(x, window_size):
    k1 = x.shape[1]/window_size
    k2 = x.shape[0]/window_size
 
+   # weight
    w = 2./(k1*k2)
 
    blocksize_x = window_size
    blocksize_y = window_size
 
-   # make sure image is divisible by window_size
+   # make sure image is divisible by window_size - doesn't matter if we cut out some pixels
    x = x[:blocksize_y*k2, :blocksize_x*k1]
 
-   start_x = 0
-   start_y = 0
    val = 0
    for l in range(k1):
       for k in range(k2):
-
          block = x[k*window_size:window_size*(k+1), l*window_size:window_size*(l+1)]
          max_ = np.max(block)
          min_ = np.min(block)
 
+         # bound checks, can't do log(0)
          if min_ == 0.0: val += 0
          elif max_ == 0.0: val += 0
          else: val += math.log(max_/min_)
@@ -120,7 +119,8 @@ def eme(x, window_size):
    Underwater Image Sharpness Measure
 '''
 def _uism(x):
-   
+
+   # get image channels
    R = x[:,:,0]
    G = x[:,:,1]
    B = x[:,:,2]
@@ -130,19 +130,90 @@ def _uism(x):
    Gs = sobel(G)
    Bs = sobel(B)
 
+   # multiply the edges detected for each channel by the channel itself
    R_edge_map = np.multiply(Rs, R)
    G_edge_map = np.multiply(Gs, G)
    B_edge_map = np.multiply(Bs, B)
 
+   # get eme for each channel
    r_eme = eme(R_edge_map, 10)
    g_eme = eme(G_edge_map, 10)
    b_eme = eme(B_edge_map, 10)
 
+   # coefficients
    lambda_r = 0.299
    lambda_g = 0.587
    lambda_b = 0.144
 
    return (lambda_r*r_eme) + (lambda_g*g_eme) + (lambda_b*b_eme)
+
+def plip_g(x,mu=1026.0):
+   return mu-x
+   return x
+
+def plip_theta(g1, g2, k):
+   g1 = plip_g(g1)
+   g2 = plip_g(g2)
+   return k*((g1-g2)/(k-g2))
+
+def plip_cross(g1, g2, gamma):
+   g1 = plip_g(g1)
+   g2 = plip_g(g2)
+   return g1+g2-((g1*g2)/(gamma))
+
+def plip_diag(c, g, gamma):
+   g = plip_g(g)
+   return gamma - (gamma * math.pow((1 - (g/gamma) ), c) )
+
+
+'''
+   Underwater image contrast measure
+'''
+def _uiconm(x, window_size):
+
+   '''
+      PLIP definitions
+      https://github.com/tkrahn108/UIQM/blob/master/src/uiconm.cpp
+      https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=5609219
+   '''
+   plip_lambda = 1026.0
+   plip_gamma  = 1026.0
+   plip_beta   = 1.0
+   plip_mu     = 1026.0
+   plip_k      = 1026.0
+   
+   # if 4 blocks, then 2x2...etc.
+   k1 = x.shape[1]/window_size
+   k2 = x.shape[0]/window_size
+
+   # weight
+   w = 1./(k1*k2)
+
+   blocksize_x = window_size
+   blocksize_y = window_size
+
+   # make sure image is divisible by window_size - doesn't matter if we cut out some pixels
+   x = x[:blocksize_y*k2, :blocksize_x*k1]
+
+   val = 0
+   for l in range(k1):
+      for k in range(k2):
+         block = x[k*window_size:window_size*(k+1), l*window_size:window_size*(l+1), :]
+         max_ = np.max(block)
+         min_ = np.min(block)
+
+         top = plip_theta(max_, min_, plip_k)
+         bot = plip_cross(max_, min_, plip_gamma)
+         print top
+         print bot
+         print
+
+         try: val += (top/bot) * math.log(top/bot)
+         except: val += 0.0
+
+
+   return plip_diag(w, val, plip_gamma)
+
 
 
 if __name__ == '__main__':
@@ -153,11 +224,16 @@ if __name__ == '__main__':
 
    image = cv2.imread(sys.argv[1])
    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-   #image = cv2.resize(image, (256,256))
    image = image.astype(np.float32)
 
-   uicm = _uicm(image)
-   uism = _uism(image)
+   uicm   = _uicm(image)
+   uism   = _uism(image)
+   uiconm = _uiconm(image, 10)
 
    print 'UICM:',uicm
    print 'UISM:',uism
+   print 'UIConM:',uiconm
+
+
+
+
